476325273eb4400417609001dbb40e4be826afb8
Matthew Salamack <mattymacho3@gmail.com>
2022-04-08 22:00:11 -0400

    Crypto Price App Tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import Coin from "./components/Coin";
 
 function App() {
+  const [listOfCoins, setListOfCoins] = useState([]);
+  const [searchWord, setSearchWord] = useState([""]);
+  const [listOfUrl, setListOfUrl] = useState([]);
+
+  useEffect(() => {
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
+      (response) => {
+        setListOfCoins(response.data.coins);
+      }
+    );
+  }, []);
+
+  const filteredCoins = listOfCoins.filter((coin) => {
+    return coin.name
+      .toLowerCase()
+      .includes(searchWord.toString().toLowerCase());
+  });
+
+  const coinUrl = listOfCoins.filter((coin) => {
+    console.log(coin.exp[0]);
+    return coin.exp[0];
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Crypto Header Bar */}
+      <div className="cryptoHeader">
+        <input
+          className="queryBox"
+          type="text"
+          placeholder="Insert coin name..."
+          onChange={(event) => {
+            setSearchWord(event.target.value);
+          }}
+        />
+      </div>
+
+      {/* Crypto Card */}
+      <div className="cryptoDisplay">
+        {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              name={coin.name}
+              icon={coin.icon}
+              price={coin.price}
+              symbol={coin.symbol}
+              exp={coin.exp[0]}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
